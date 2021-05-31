35aef43915cf4a569f144098427381529488a32e
Andre Luis Mazurkiewitz <andre@Andres-MacBook-Pro.local>
2021-05-31 14:49:59 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fugiat
+        culpa, vitae vero veritatis repellat quo iure exercitationem nesciunt at
+        delectus, pariatur aut illum ipsam. Aperiam ex aliquid facilis
+        distinctio dolor, adipisci debitis atque deserunt reiciendis itaque,
+        minima cum quo.
+      </p>
+    </section>
+  );
+};
+
+export default About;
